1d65cdb9aed845fea4b6a0d0cfbbf04aeda794cb
Valentina Antanovich <valentina8antanovich@gmail.com>
2021-03-13 23:31:13 +0300

    Task Human Readable Number solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function toReadable (number) {
-  
+let result = '';    
+  let arr = String(number).split('');
+  let one = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  let two = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+if (number == 0) {return 'zero'}   
+    
+if (arr.length == 3) { 
+    result = `${one[Number(arr[0])]} hundred `;
+    arr.shift()}
+if (arr.length == 2) { 
+    if (Number(arr[0]) >= 2 ) {
+        result = result + `${two[Number(arr[0])]} `;
+        arr.shift();
+        } else {
+            if (Number(arr[0]) == 1 ) {
+                result = result + `${one[Number(arr.join(''))]}`;
+            } else {arr.shift()};    
+        }
+    }
+if (arr.length == 1) { 
+    
+    result = result + `${one[Number(arr[0])]}`;
+  }
+
+
+return result.trim();      
 }
